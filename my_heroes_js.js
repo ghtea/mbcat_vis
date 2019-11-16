5fb496eb4891929ea2520fcb26f04007db794e22
ghtea <47841931+ghtea@users.noreply.github.com>
2019-11-16 22:14:28 +0900

    Update my_heroes_js.js

--- my_heroes_js.js
+++ my_heroes_js.js
@@ -45,7 +45,7 @@ d3.csv("my_heroes_csv.csv", function(data) {
     .append("circle")
       .attr("cx", function (d) { return x(d.Like); } )
       .attr("cy", function (d) { return y(d.Skilled); } )
-      .attr("r",  function (d) { return y(d.Played); } )
+      .attr("r",  function (d) { return y(d.Played) / 20 ; } )
       .style("fill", function (d) { return color(d.Role); } )
       .style("opacity", function (d) { return color(d.Played); } )
 })
