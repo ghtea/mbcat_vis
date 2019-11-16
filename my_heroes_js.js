e52935c926aa6fb633e8e4fd29d190bce96b7134
ghtea <47841931+ghtea@users.noreply.github.com>
2019-11-16 20:54:07 +0900

    Update my_heroes_js.js

--- my_heroes_js.js
+++ my_heroes_js.js
@@ -1,3 +1,51 @@
-d3.csv("my_heroes_csv.csv", function(data){
-    console.log(data);
-});
+
+
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#chart")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+//Read the data
+d3.csv("my_heroes_csv.csv", function(data) {
+
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([4, 8])
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 9])
+    .range([ height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // Color scale: give me a specie name, I return a color
+  var color = d3.scaleOrdinal()
+    .domain(["Tank", "Bruiser", "Support", "Healer", "Ranged Assassin", "Melee Assassin" ])
+    .range([ "#73C8FF", "#FF965A", "#73C8FF", "#64C8CD", "#FFCD3C", "#FF5F69"])
+
+  // Add dots
+  svg.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("cx", function (d) { return x(d.Like); } )
+      .attr("cy", function (d) { return y(d.Skilled); } )
+      .attr("r", 5)
+      .style("fill", function (d) { return color(d.Role) } )
+
+})
